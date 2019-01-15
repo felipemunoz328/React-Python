92ff932252d5398043e873823122f5034efd1601
Tomás Flores <tomas.flores.a@gmail.com>
2019-01-15 09:58:06 -0300

    borrado

--- swagger_server/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-
-# flake8: noqa
-from __future__ import absolute_import
-# import models into model package
-from swagger_server.models.estimated_time import EstimatedTime
-from swagger_server.models.get_lat_lon import GetLatLon
